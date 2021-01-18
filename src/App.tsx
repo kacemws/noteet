6eac53e66e473e6431a485d0b8939e32e2d1b974
kacemws <berras.belkacem@gmail.com>
2021-01-18 10:47:55 +0100

    default styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h2>Boilerplate!</h2>
     </div>
   );
 }
