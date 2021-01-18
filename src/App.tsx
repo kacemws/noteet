e493e6e5c8183e7870106cdad1e03af02370ea15
kacemws <berras.belkacem@gmail.com>
2021-01-18 14:27:01 +0100

    removed side menu's props from app file

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,8 @@ import { Panel } from "./Components/Panel";
 import { Sidemenu } from "./Components/SideMenu";
 import "./Styles/App.scss";
 import "./Styles/colors.scss";
-import plus from "./Assets/plus.svg";
-import { useState } from "react";
-import { useSpring, animated } from "react-spring";
 
 function App() {
-  const [open, setOpen] = useState(false);
-  const buttonProps = useSpring({
-    transform: open ? "rotate(45deg)" : "rotate(360deg)",
-  });
-  const blockProps = useSpring({
-    marginTop: open ? 0 : -48,
-  });
-  const selectorProps = useSpring({
-    opacity: open ? 1 : 0,
-  });
   return (
     <div
       style={{
@@ -25,43 +12,7 @@ function App() {
         display: "flex",
       }}
     >
-      <Sidemenu>
-        <div className="logo">Noteet</div>
-        <div className="notes-container">
-          <div className="add-button">
-            <button
-              className="addNote"
-              onClick={() => {
-                setOpen(!open);
-              }}
-            >
-              <animated.img src={plus} style={buttonProps} alt="Plus Icon" />
-            </button>
-          </div>
-          <animated.div className="items" style={blockProps}>
-            <animated.div
-              className="selector first"
-              style={selectorProps}
-            ></animated.div>
-            <animated.div
-              className="selector second"
-              style={selectorProps}
-            ></animated.div>
-            <animated.div
-              className="selector third"
-              style={selectorProps}
-            ></animated.div>
-            <animated.div
-              className="selector fourth"
-              style={selectorProps}
-            ></animated.div>
-            <animated.div
-              className="selector fifth"
-              style={selectorProps}
-            ></animated.div>
-          </animated.div>
-        </div>
-      </Sidemenu>
+      <Sidemenu />
       <Panel></Panel>
     </div>
   );
