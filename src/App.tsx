1cff4856dd2a3f7e8a0b40912e25831109017632
kacemws <berras.belkacem@gmail.com>
2021-01-18 13:18:50 +0100

    animating the sidemenu

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,12 @@ function App() {
   const buttonProps = useSpring({
     transform: open ? "rotate(45deg)" : "rotate(360deg)",
   });
+  const blockProps = useSpring({
+    marginTop: open ? 0 : -48,
+  });
+  const selectorProps = useSpring({
+    opacity: open ? 1 : 0,
+  });
   return (
     <div
       style={{
@@ -32,13 +38,28 @@ function App() {
               <animated.img src={plus} style={buttonProps} alt="Plus Icon" />
             </button>
           </div>
-          <div className="items">
-            <div className="selector first"></div>
-            <div className="selector second"></div>
-            <div className="selector third"></div>
-            <div className="selector fourth"></div>
-            <div className="selector fifth"></div>
-          </div>
+          <animated.div className="items" style={blockProps}>
+            <animated.div
+              className="selector first"
+              style={selectorProps}
+            ></animated.div>
+            <animated.div
+              className="selector second"
+              style={selectorProps}
+            ></animated.div>
+            <animated.div
+              className="selector third"
+              style={selectorProps}
+            ></animated.div>
+            <animated.div
+              className="selector fourth"
+              style={selectorProps}
+            ></animated.div>
+            <animated.div
+              className="selector fifth"
+              style={selectorProps}
+            ></animated.div>
+          </animated.div>
         </div>
       </Sidemenu>
       <Panel></Panel>
