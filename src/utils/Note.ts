6e1306a8d52817cb8d7fdd0b5b530328afcb636a
kacemws <berras.belkacem@gmail.com>
2021-01-21 20:06:12 +0100

    added a date attribute to the note

--- src/utils/Note.ts
+++ src/utils/Note.ts
@@ -1,8 +1,10 @@
 export class Note {
   color: string;
   note: string;
+  date: Date;
   constructor(color: string, note: string) {
     this.color = color;
     this.note = note;
+    this.date = new Date();
   }
 }
