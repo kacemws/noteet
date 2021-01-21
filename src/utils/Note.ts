eae79e414c336d1b7ba65ae95a62de07df3cf6c3
kacemws <berras.belkacem@gmail.com>
2021-01-21 21:51:13 +0100

    added id field to the note object

--- src/utils/Note.ts
+++ src/utils/Note.ts
@@ -1,8 +1,12 @@
+import { makeid } from "./random";
+
 export class Note {
+  id: string;
   color: string;
   note: string;
   date: Date;
   constructor(color: string, note: string) {
+    this.id = makeid();
     this.color = color;
     this.note = note;
     this.date = new Date();
