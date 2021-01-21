128e530b0379e1fccf5fd8837f66f3bc4979a6de
kacemws <berras.belkacem@gmail.com>
2021-01-21 19:21:39 +0100

    adding the save icon and displaying icons dynamically

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import edit from "../Assets/edit.svg";
+import save from "../Assets/save.svg";
 import "../Styles/Components/Card.scss";
 import { Note } from "../utils/Note";
 
@@ -11,6 +12,7 @@ interface props {
 }
 export const Card: React.FC<props> = ({ note, index, ...rest }) => {
   const [animate, setAnimate] = useState(false);
+  const [value, setValue] = useState(note.note);
   useEffect(() => {
     if (index == 0) {
       console.log("will animate " + note.color + " : " + index);
@@ -40,8 +42,11 @@ export const Card: React.FC<props> = ({ note, index, ...rest }) => {
       style={index ? cardProps : firstProp}
     >
       <textarea
-        // type="text"
         placeholder="Type your note"
+        value={value}
+        onChange={({ target }) => {
+          setValue(target.value);
+        }}
       ></textarea>
       <div className="footer">
         <div className="date">
@@ -49,7 +54,7 @@ export const Card: React.FC<props> = ({ note, index, ...rest }) => {
         </div>
         <div className="edit">
           <button>
-            <img src={edit} alt="Edit Icon" />
+            <img src={save} alt="Edit Icon" />
           </button>
         </div>
       </div>
