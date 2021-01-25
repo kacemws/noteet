183a7e885f1dfb07eeda6cd9e9240255e8a93516
kacemws <berras.belkacem@gmail.com>
2021-01-25 15:11:23 +0100

    refreshing token on app init ( if token is expired)

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,112 @@ import Signin from "./Views/SignIn";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Signup from "./Views/Signup";
 
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+import { refreshExpiredToken, setAuthToken } from "./api/axios";
+
 function App() {
+  /*Token*/
+  const [token, setToken] = useState<string | null>(
+    Cookies.get("accessToken") ?? ""
+  ); // the actual token;
+  /*Token*/
+
+  const [interval, setIntervalHandler] = useState<number>(0);
+
+  const [fetchingToken, setFetchingToken] = useState(true); // a variable that checks wheter we are refreshing the token or not
+
+  useEffect(() => {
+    window.clearInterval(interval); // to clear all sets interval for the check cookie function
+
+    let cookieToken = Cookies.get("accessToken");
+    let refreshToken = localStorage.getItem("refreshToken");
+
+    /*to know wheter to ask for a new token or not*/
+    if (
+      cookieToken == undefined &&
+      refreshToken != undefined &&
+      refreshToken.length > 0
+    ) {
+      // if cookie expired
+      refreshExpiredToken(refreshToken).then((res) => {
+        console.log(res);
+
+        var in15minutes = new Date(new Date().getTime() + 900000);
+
+        Cookies.set("accessToken", res.data.accessToken, {
+          expires: in15minutes,
+        });
+        localStorage.setItem("refreshToken", res.data.refreshToken);
+
+        setAuthToken(res.data.accessToken);
+        setToken(res.data.accessToken);
+        //set the fetched credentials where needed : the cookie, local storage , the axios instance and finally the token variable that is local to the app file
+
+        setFetchingToken(false);
+        //refreshing done!!!
+
+        let handler = window.setInterval(checkCookie, 100); // setting an interval each 100ms (0.1 seconds) to execute the check cookie function ( check line 58)
+        setIntervalHandler(handler);
+      });
+    } else {
+      // if both undefined of both defined ( refresh_token && access_token)
+      setFetchingToken(false);
+      let handler = window.setInterval(checkCookie, 100); // setting an interval each 100ms (0.1 seconds) to execute the check cookie function ( check line 58)
+      setIntervalHandler(handler);
+    }
+  }, []);
+
+  const checkCookie = (function () {
+    // function to check wheter cookie changed or not
+
+    var lastCookie = document.cookie; // 'static' memory between function calls (old cookie)
+
+    return function () {
+      var currentCookie = document.cookie; // current cookie;
+
+      if (currentCookie != lastCookie) {
+        // if cookie changed
+        lastCookie = currentCookie;
+        console.log("changed a cookie!");
+        let token =
+          Cookies.get("accessToken") != undefined
+            ? Cookies.get("accessToken")
+            : "no token";
+        if (token == "no token") {
+          console.log("token expired...");
+        }
+        setToken(token ?? ""); //set token stored inside the new cookie
+      }
+    };
+  })();
+
+  if (token != "") {
+    setAuthToken(token);
+  }
+
+  let routes; // declaring the routes
+
+  if (token === "") {
+    routes = (
+      <Switch>
+        <Route path="/sign-up" component={Signup} />
+        <Route exact path="/" component={Signin} />
+        {/*done*/}
+        <Route path="*">
+          <h1>404 error</h1>
+        </Route>
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route exact path="/" render={() => <NoteView />} />
+        <Route path="" render={() => <h1>404 error</h1>} />
+      </Switch>
+    );
+  }
+
   return (
     <Router>
       <div
@@ -14,7 +119,7 @@ function App() {
           minHeight: "100vh",
         }}
       >
-        <Switch>
+        {/* <Switch>
           <Route exact path="/">
             <Signin />
           </Route>
@@ -24,7 +129,8 @@ function App() {
           <Route path="/notes">
             <NoteView />
           </Route>
-        </Switch>
+        </Switch> */}
+        {routes}
       </div>
     </Router>
   );
