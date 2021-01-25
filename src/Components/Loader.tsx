906b22db51da97fc7505acd2bc5177ce875955d4
kacemws <berras.belkacem@gmail.com>
2021-01-25 15:26:31 +0100

    adding a loading component

---
+++ src/Components/Loader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../Styles/Components/Loader.scss";
+function Loader() {
+  return (
+    <div className="container">
+      <div className="loader" />
+    </div>
+  );
+}
+
+export default Loader;
